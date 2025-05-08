79db54b0cbbf5c875b4b4df34170e2ce794ffd44
kanishkmishra-afk <kanishkm794@gmail.com>
2025-05-08 18:57:35 +0530

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import image from '../assets/image.jpeg'
+import logo from '../assets/logo.svg'
 function Logo({width='100px'}){
     return(
-        <div>Logo</div>
+        <img src={logo} alt="logo" style={{width}} />
     )
 }
 export default Logo
