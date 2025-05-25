a5e1a1a4447fa30c54d9b9bc368a5770f0685ed8
kanishkmishra-afk <kanishkm794@gmail.com>
2025-05-25 16:09:30 +0530

    upgradition

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -3,13 +3,17 @@ import {useDispatch} from 'react-redux'
 import { logout } from '../../store/authSlice'
 import authService from "../../appwrite/auth";
 import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
+import { useNavigate } from "react-router-dom";
 
 function LogoutBtn(){
     const dispatch=useDispatch();
+    const navigate=useNavigate();
 
     const logoutHandler=()=>{
         authService.logout().then(()=>{
             dispatch(logout())
+            navigate('/')
+            window.location.reload();
         }).catch((error)=>{
             throw error;
         })
@@ -18,6 +22,7 @@ function LogoutBtn(){
         <button className={`flex items-center w-full px-3 py-2 text-sm text-gray-700 rounded-md hover:text-red-600 transition-colors`}
         iconClassName={`h-5 w-5 mr-2 text-gray-400 hover:text-red-400`}
         onClick={logoutHandler}
+        
         >
             <ArrowDownCircleIcon className="h-5 w-5 mr-2" />
             Sign Out</button>
